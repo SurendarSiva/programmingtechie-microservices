82da3db82a1d6f4b7da2c1d9adf912531af094cb
SurendarSiva <surendarkrt@gmail.com>
2023-05-26 16:01:37 +0530

    2nd commit

--- product-service/src/main/java/com/programmingtechie/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/programmingtechie/productservice/controller/ProductController.java
@@ -1,17 +1,22 @@
 package com.programmingtechie.productservice.controller;
 
 import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-
+            productService.createProduct(productRequest);
     }
 
 }
