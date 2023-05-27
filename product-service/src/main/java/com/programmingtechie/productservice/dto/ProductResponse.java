667d12aa3aefc6a756560f6d1b7fcab4d7e8e4b9
SurendarSiva <51986062+SurendarSiva@users.noreply.github.com>
2023-05-27 21:09:40 +0530

    Add files via upload

--- product-service/src/main/java/com/programmingtechie/productservice/dto/ProductResponse.java
+++ product-service/src/main/java/com/programmingtechie/productservice/dto/ProductResponse.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
-@Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductRequest {
+@Builder
+@Data
+public class ProductResponse {
+
+    private String id;
     private String name;
     private String description;
     private BigDecimal price;
